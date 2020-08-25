4006b4f21a74a8c0e227eb81b194b62f9eee8cd4
Nicholas <higherfuryx@mail.com>
2020-08-25 14:10:20 -0500

    Get Methods

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -34,8 +35,25 @@ namespace RestaurantRaterAPI.Controllers
 
         // -- Read (GET)
         // Get by ID
+        [HttpGet]
+        public async Task<IHttpActionResult> GetByID(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+            return NotFound();
+        }
 
         // Get All
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
 
         // -- Update (PUT)
 
